39c7e4f667229c23aec42f7fcad62d137a69f152
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-04-17 06:31:20 -0500

    Converted this example into a Java Eclipse project.

--- ex/BuggyQueue/src/Main.java
+++ ex/BuggyQueue/src/Main.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public class Main
 { 
+   /* 
+    * @class BuggyQueue
+    *
+    * @brief This class doesn't work properly when accessed via
+    *        multiple threads since it's not synchronized properly.
+    *
+    */
     static class BuggyQueue
     {
         // Resizable-array implementation.
@@ -18,15 +25,24 @@ public class Main
 
     static int mMaxIterations = 100;
 
+    /*
+     * Main entry point into the BuggyQueue class.
+     */
     public static void main(String argv[]) {
         final BuggyQueue buggyQueue = new BuggyQueue();
 
+        /* 
+         * Create a producer thread.
+         */
         Thread producer = 
             new Thread(new Runnable(){ 
                     public void run(){ 
                         for(int i = 0; i < mMaxIterations; i++)
                             buggyQueue.put(Integer.toString(i)); 
                     }});
+        /* 
+         * Create a consumer thread.
+         */
         Thread consumer =
             new Thread(new Runnable(){
                     public void run(){ 
@@ -34,6 +50,9 @@ public class Main
                             System.out.println(buggyQueue.take());
                     }});
 
+        /* 
+         * Run both threads concurrently.
+         */
         consumer.start();
         producer.start();
     }
