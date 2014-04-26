ab34d4ea49a78fe8bc83e464d07ff471d36bd75a
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-04-26 09:45:23 -0500

    Updated AtomicLongTest.java to use the Command pattern to simplify the code.

--- assignments/assignment0/src/AtomicLongTest.java
+++ assignments/assignment0/src/AtomicLongTest.java
@@ -14,24 +14,69 @@ class AtomicLongTest
     /**
      * Number of iterations to run the test.
      */
-    static final long mMaxIterations = 100000000;
+    final static long mMaxIterations = 100000000;
 
     /**
      * Barrier synchronizer that controls when the two threads start
      * running the test.
      */
-    static CyclicBarrier mStartBarrier = new CyclicBarrier(2);
+    final static CyclicBarrier mStartBarrier = new CyclicBarrier(2);
 
     /**
      * Barrier synchronizer that controls when the main thread can
      * return.
      */
-    static CountDownLatch mStopLatch = new CountDownLatch(2);
+    final static CountDownLatch mStopLatch = new CountDownLatch(2);
 
     /**
      * An instance of our implementation of AtomicLong.
      */
-    static AtomicLong mCounter = new AtomicLong(0);
+    final static AtomicLong mCounter = new AtomicLong(0);
+
+    /**
+     * @brief This class runs the test by invoking a command each time
+     *        through the loop.
+     */
+    static class RunTest implements Runnable
+    { 
+        /**
+         * A Command that determines what operation is done within the
+         * loop.
+         */
+        private Runnable mCommand;
+
+        /**
+         * Store the command in a data member field.
+         */
+        RunTest(Runnable command) {
+            mCommand = command;
+        }
+        
+        /**
+         * Run the command within a loop.
+         */
+        public void run() {
+            try
+                {
+                    /**
+                     * Wait for both Threads to start running before
+                     * beginning the loop.
+                     */
+                    mStartBarrier.await();
+
+                    for (int i = 0; i < mMaxIterations; ++i)
+                        mCommand.run();
+
+                    /**
+                     * Inform the main thread that we're done.
+                     */
+                    mStopLatch.countDown();
+                }
+            catch (Exception e) {
+                System.out.println("problem here");
+            }
+        }
+    }
 
     /**
      * Main entry point method that runs the test.
@@ -41,58 +86,28 @@ class AtomicLongTest
             System.out.println("Starting AtomicLongTest");
 
             /**
-             * Start a Thread whose Runnable decrements the AtomicLong
-             * mMaxIterations number of times.
+             * A Runnable command that decrements the mCounter.
              */
-            new Thread(new Runnable()
-                { public void run() {
-                    try
-                        {
-                            /**
-                             * Wait for both Threads to start running
-                             * before beginning the loop.
-                             */
-                            mStartBarrier.await();
-
-                            for (int i = 0; i < mMaxIterations; ++i)
-                                mCounter.getAndDecrement();
-
-                            /**
-                             * Inform the main thread that we're done.
-                             */
-                            mStopLatch.countDown();
-                        }
-                    catch (Exception e)
-                        {
-                            System.out.println("problem here");
-                        }
-                }
-                }).start();
+            final Runnable decrementCommand =
+                new Runnable() { public void run() { mCounter.getAndDecrement(); } };
+            
+            /**
+             * A Runnable command that decrements the mCounter.
+             */
+            final Runnable incrementCommand =
+                new Runnable() { public void run() { mCounter.getAndIncrement(); } };
 
             /**
-             * Start a Thread whose Runnable increments the AtomicLong
-             * mMaxIterations number of times.
+             * Start a Thread whose Runnable command decrements the
+             * AtomicLong mMaxIterations number of times.
              */
-            new Thread(new Runnable()
-                { public void run() {
-                    try {
-                        /**
-                         * Wait for both Threads to start running
-                         * before beginning the loop.
-                         */
-                        mStartBarrier.await();
+            new Thread(new RunTest(decrementCommand)).start();
 
-                        for (int i = 0; i < mMaxIterations; ++i)
-                            mCounter.getAndIncrement();
-
-                        /**
-                         * Inform the main thread that we're done.
-                         */
-                        mStopLatch.countDown();
-                    }
-                    catch (Exception e) { }
-                }
-                }).start();
+            /**
+             * Start a Thread whose Runnable command increments the
+             * AtomicLong mMaxIterations number of times.
+             */
+            new Thread(new RunTest(incrementCommand)).start();
 
             /**
              * Barrier synchronizer that waits for both worker threads
