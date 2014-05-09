5d679f1a106b6ca0806ea46a8615e7fdc7e81b2c
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-05-09 10:10:25 -0500

    Enhanced the SynchronizedQueue regression tests.

--- assignments/week-1-assignment-0/src/edu/vuum/mooca/MainTestDriver.java
+++ assignments/week-1-assignment-0/src/edu/vuum/mooca/MainTestDriver.java
@@ -6,46 +6,45 @@ import edu.vuum.mooca.SynchronizedQueue.QueueAdapter;
 import edu.vuum.mooca.SynchronizedQueue.SynchronizedQueueResult;
 
 public class MainTestDriver {
+    /**
+     * Run the test for the queue parameter.
+     */
+    static void testQueue(String testName,
+                          QueueAdapter<Integer> queue) {
+        // Ask the SynchronizedQueue to test itself using the queue
+        // parameter.
+        SynchronizedQueueResult result = SynchronizedQueue.testQueue(queue);
 
-	/**
-	 * Run the test for the queue parameter.
-	 */
-	static void testQueue(String testName, QueueAdapter<Integer> queue) {
+        String resultString = "*** Test " + testName + " ";
+        if (result != SynchronizedQueueResult.RAN_PROPERLY) 
+            resultString += "Failed because: " + result.getString();
+        else 
+            resultString += "Passed.";
 
-		SynchronizedQueueResult result = SynchronizedQueue.testQueue(queue);
+        System.out.println(resultString);
+    }
 
-		String resultString = "*** Test " + testName + " ";
-		if (result != SynchronizedQueueResult.RAN_PROPERLY) {
-			resultString += "Failed because: " + result.getString();
-		} else {
-			resultString += "Passed.";
-		}
+    /**
+     * Main entry point method into the test program.
+     */
+    public static void main(String argv[]) {
+        System.out.println("Starting SynchronizedQueueTest");
+        // Indicate how big the queue should be, which should be
+        // smaller than the number of iterations to induce blocking
+        // behavior.
+        int queueSize = SynchronizedQueue.mMaxIterations / 10;
 
-		System.out.println(resultString);
+        // Test the ArrayBlockingQueue, which should pass the test.
+        SynchronizedQueue.mQueue = new QueueAdapter<Integer>(
+                                                             new ArrayBlockingQueue<Integer>(queueSize));
+        testQueue("ArrayBlockingQueue", SynchronizedQueue.mQueue);
 
-	}
+        // Test the BuggyBlockingQueue, which should fail the test.
+        SynchronizedQueue.mQueue = new QueueAdapter<Integer>(
+                                                             new BuggyBlockingQueue<Integer>(queueSize));
+        testQueue("BuggyBlockingQueue", SynchronizedQueue.mQueue);
 
-	/**
-	 * Main entry point method into the test program.
-	 */
-	public static void main(String argv[]) {
-		System.out.println("Starting SynchronizedQueueTest");
-		// Indicate how big the queue should be, which should be
-		// smaller than the number of iterations to induce blocking
-		// behavior.
-		int queueSize = SynchronizedQueue.mMaxIterations / 10;
-
-		// Test the ArrayBlockingQueue, which should pass the test.
-		SynchronizedQueue.mQueue = new QueueAdapter<Integer>(
-				new ArrayBlockingQueue<Integer>(queueSize));
-		testQueue("ArrayBlockingQueue", SynchronizedQueue.mQueue);
-
-		// Test the BuggyBlockingQueue, which should fail the test.
-		SynchronizedQueue.mQueue = new QueueAdapter<Integer>(
-				new BuggyBlockingQueue<Integer>(queueSize));
-		testQueue("BuggyBlockingQueue", SynchronizedQueue.mQueue);
-
-		System.out.println("Finishing SynchronizedQueueTest");
-	}
+        System.out.println("Finishing SynchronizedQueueTest");
+    }
 
 }
