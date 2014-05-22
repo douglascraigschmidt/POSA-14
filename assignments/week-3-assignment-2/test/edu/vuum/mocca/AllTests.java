2dfc9bc907a2862fc04970c75829e966d754d968
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-05-22 15:26:00 -0500

    Added JUnit tests for week-3-assignment-2.

--- assignments/week-3-assignment-2/test/edu/vuum/mocca/AllTests.java
+++ assignments/week-3-assignment-2/test/edu/vuum/mocca/AllTests.java
@@ -5,8 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({PalantirManagerUnitTest.class, SimpleAtomicLongUnitTest.class,
-		SimpleSemaphoreUnitTest.class})
+@SuiteClasses({PalantirManagerUnitTest.class,
+               SimpleAtomicLongUnitTest.class,
+               SimpleSemaphoreUnitTest.class})
 public class AllTests {
-
 }
