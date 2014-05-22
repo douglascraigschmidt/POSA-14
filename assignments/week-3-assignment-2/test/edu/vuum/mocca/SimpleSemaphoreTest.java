a2c5a8a0a8d3de085bb07a61cef048389c597715
Michael A. Walker <mwalker@isis.vanderbilt.edu>
2014-05-22 03:44:53 -0500

    Added JUnit 4 and ./test/ to classpath and first JUnit test case

---
+++ assignments/week-3-assignment-2/test/edu/vuum/mocca/SimpleSemaphoreTest.java
@@ -0,0 +1,49 @@
+package edu.vuum.mocca;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SimpleSemaphoreTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSimpleSemaphore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcquire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcquireUninterruptibly() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRelease() {
+		fail("Not yet implemented");
+	}
+
+}
