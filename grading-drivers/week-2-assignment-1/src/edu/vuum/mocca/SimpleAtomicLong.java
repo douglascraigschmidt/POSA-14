a01e93d4c2eec7c521c9d2c829203c66dc48e808
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-05-23 17:08:24 -0500

    Added the SimpleAtomicLong.java file back to the grading driver.

---
+++ grading-drivers/week-2-assignment-1/src/edu/vuum/mocca/SimpleAtomicLong.java
@@ -0,0 +1,106 @@
+package edu.vuum.mocca;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @class SimpleAtomicLong
+ *
+ * @brief This class implements a subset of the
+ *        java.util.concurrent.atomic.SimpleAtomicLong class using a
+ *        ReentrantReadWriteLock to illustrate how they work.
+ */
+class SimpleAtomicLong
+{
+    /**
+     * The value that's manipulated atomically via the methods.
+     */
+    private long mValue;
+    
+    /**
+     * The ReentrantReadWriteLock used to serialize access to mValue.
+     */
+
+    // TODO -- you fill in here by replacing the null with an
+    // initialization of ReentrantReadWriteLock.
+    private ReentrantReadWriteLock mRWLock = null;
+
+    /**
+     * Creates a new SimpleAtomicLong with the given initial value.
+     */
+    public SimpleAtomicLong(long initialValue)
+    {
+        // TODO -- you fill in here
+    }
+
+    /**
+     * @brief Gets the current value.
+     * 
+     * @returns The current value
+     */
+    public long get()
+    {
+        long value;
+
+        // TODO -- you fill in here
+
+        return value;
+    }
+
+    /**
+     * @brief Atomically decrements by one the current value
+     *
+     * @returns the updated value
+     */
+    public long decrementAndGet()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+
+        return value;
+    }
+
+    /**
+     * @brief Atomically increments by one the current value
+     *
+     * @returns the previous value
+     */
+    public long getAndIncrement()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+
+        return value;
+    }
+
+    /**
+     * @brief Atomically decrements by one the current value
+     *
+     * @returns the previous value
+     */
+    public long getAndDecrement()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+
+        return value;
+    }
+
+    /**
+     * @brief Atomically increments by one the current value
+     *
+     * @returns the updated value
+     */
+    public long incrementAndGet()
+    {
+        long value = 0;
+
+        // TODO -- you fill in here
+
+        return value;
+    }
+}
+
