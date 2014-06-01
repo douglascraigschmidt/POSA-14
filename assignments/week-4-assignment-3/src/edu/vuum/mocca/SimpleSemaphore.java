b6cb29ad3eebadc31daaa3ecdc503f77091d4870
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-06-01 08:27:29 -0500

    Renamed the package.

---
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -0,0 +1,66 @@
+package edu.vuum.mocca;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @class SimpleSemaphore
+ * 
+ * @brief This class provides a simple counting semaphore implementation using
+ *        Java a ReentrantLock and a ConditionObject (which is accessed via a
+ *        Condition). It must implement both "Fair" and "NonFair" semaphore
+ *        semantics, just liked Java Semaphores.
+ */
+public class SimpleSemaphore {
+    /**
+     * Define a ReentrantLock to protect the critical section.
+     */
+    // TODO - you fill in here
+
+    /**
+     * Define a Condition that waits while the number of permits is 0.
+     */
+    // TODO - you fill in here
+
+    /**
+     * Define a count of the number of available permits.
+     */
+    // TODO - you fill in here. Make sure that this data member will
+    // ensure its values aren't cached by multiple Threads..
+
+    public SimpleSemaphore(int permits, boolean fair) {
+        // TODO - you fill in here to initialize the SimpleSemaphore,
+        // making sure to allow both fair and non-fair Semaphore
+        // semantics.
+    }
+
+    /**
+     * Acquire one permit from the semaphore in a manner that can be
+     * interrupted.
+     */
+    public void acquire() throws InterruptedException {
+        // TODO - you fill in here.
+    }
+
+    /**
+     * Acquire one permit from the semaphore in a manner that cannot be
+     * interrupted.
+     */
+    public void acquireUninterruptibly() {
+        // TODO - you fill in here.
+    }
+
+    /**
+     * Return one permit to the semaphore.
+     */
+    void release() {
+        // TODO - you fill in here.
+    }
+
+    /**
+     * Return the number of permits available.
+     */
+    public int availablePermits() {
+        // TODO - you fill in here to return the correct result
+    	return 0;
+    }
+}
