559dcae8f1783d95447a8c54fd25a5e3beb0e67b
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-06-05 10:52:37 -0500

    Enhanced the documentation.

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
@@ -83,14 +83,14 @@ public class PingPongRight {
         }
 
         /**
-         * Method for ping/pong acquire.
+         * Method for acquiring the appropriate SimpleSemaphore.
          */
         private void acquire() {
             // TODO fill in here
         }
 
         /**
-         * Method for ping/pong release.
+         * Method for releasing the appropriate SimpleSemaphore.
          */
         private void release() {
             // TODO fill in here
