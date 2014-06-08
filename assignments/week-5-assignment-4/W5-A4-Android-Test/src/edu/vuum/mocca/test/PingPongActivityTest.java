45ddf98f3e9828693e6b18e53c0da83d119bf77f
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-06-08 07:47:45 -0500

    Added back the command-line variant.

--- assignments/week-5-assignment-4/W5-A4-Android-Test/src/edu/vuum/mocca/test/PingPongActivityTest.java
+++ assignments/week-5-assignment-4/W5-A4-Android-Test/src/edu/vuum/mocca/test/PingPongActivityTest.java
@@ -9,66 +9,73 @@ import com.robotium.solo.Solo;
 
 import edu.vuum.mocca.PingPongActivity;
 
+/**
+ * @class PingPongActivityTest
+ *
+ * @brief Implements a Robotium test for the PingPongActivity.
+ */
 public class PingPongActivityTest
-		extends
-			ActivityInstrumentationTestCase2<PingPongActivity> {
-	public PingPongActivityTest() {
-		super(PingPongActivity.class);
-	}
+       extends ActivityInstrumentationTestCase2<PingPongActivity> {
+    /**
+     * Constructor initializes the superclass.
+     */
+    public PingPongActivityTest() {
+        super(PingPongActivity.class);
+    }
 
-	/**
-	 * This is the handle for Robotium, which allows us to interact with the UI.
-	 */
-	Solo mSolo;
+    /**
+     * This is the handle for Robotium, which allows us to interact
+     * with the UI.
+     */
+    Solo mSolo;
 
-	/**
-	 * The context of this project, not the target project.
-	 */
-	Context mContext;
+    /**
+     * The context of this project, not the target project.
+     */
+    Context mContext;
 
-	Button playButton_;
-	TextView outputTextView_;
+    Button playButton_;
+    TextView outputTextView_;
 
-	/**
-	 * Called before each test is run to perform the initialization.
-	 */
-	public void setUp() throws Exception {
-		super.setUp();
+    /**
+     * Called before each test is run to perform the initialization.
+     */
+    public void setUp() throws Exception {
+        super.setUp();
 
-		// Setup Robotium and get the EditText View.
-		mSolo = new Solo(getInstrumentation(), getActivity());
+        // Setup Robotium and get the EditText View.
+        mSolo = new Solo(getInstrumentation(), getActivity());
 
-		mContext = getInstrumentation().getContext();
+        mContext = getInstrumentation().getContext();
 
-		playButton_ = (Button) mSolo.getView(edu.vuum.mocca.R.id.play_button);
+        playButton_ = (Button) mSolo.getView(edu.vuum.mocca.R.id.play_button);
 
-		outputTextView_ = (TextView) mSolo
-				.getView(edu.vuum.mocca.R.id.pingpong_output);
+        outputTextView_ = (TextView) mSolo
+            .getView(edu.vuum.mocca.R.id.pingpong_output);
 
-		getInstrumentation().callActivityOnStart(getActivity());
-		getInstrumentation().callActivityOnResume(getActivity());
-	}
+        getInstrumentation().callActivityOnStart(getActivity());
+        getInstrumentation().callActivityOnResume(getActivity());
+    }
 
-	// No need for @Test because of use of ActivityInstrumentationTestCase2
-	// which makes each method in this class a @Test method
-	// TODO find better explanation, 100% proper
-	public void testPlayPingPongButtonPress() throws Exception {
+    /**
+     * No need for @Test because of use of
+     * ActivityInstrumentationTestCase2 which makes each method in
+     * this class a @Test method TODO find better explanation, 100%
+     * proper.
+     */
+    public void testPlayPingPongButtonPress() throws Exception {
+        Thread.sleep(TestOptions.ACCEPTABLE_STARTUP_LENGTH);
 
-		Thread.sleep(TestOptions.ACCEPTABLE_STARTUP_LENGTH);
+        // Asserttrue(outputTextView_.isAttachedToWindow() == true);
+        assertTrue(outputTextView_.getText().length() == 0);
 
-		// assertTrue(outputTextView_.isAttachedToWindow() == true);
-		assertTrue(outputTextView_.getText().length() == 0);
+        // Click on the 'play' button
+        mSolo.clickOnView(mSolo.getView(edu.vuum.mocca.R.id.play_button));
 
-		// click on the 'play' button
-		mSolo.clickOnView(mSolo.getView(edu.vuum.mocca.R.id.play_button));
+        // wait for the threads to execute
+        Thread.sleep(TestOptions.ACCEPTABLE_RUNTIME_LENGTH);
 
-		// wait for the threads to execute
-		Thread.sleep(TestOptions.ACCEPTABLE_RUNTIME_LENGTH);
-
-		assertTrue(outputTextView_.getText().toString()
-				.equals(TestOptions.ANDROID_TEXTVIEW));
-
-	}
-
-	
+        assertTrue(outputTextView_.getText().toString()
+                   .equals(TestOptions.ANDROID_TEXTVIEW));
+    }
 }
---
+++ assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/ConsolePlatformStrategy.java
@@ -0,0 +1,73 @@
+package edu.vuum.mocca;
+
+import java.util.concurrent.CountDownLatch;
+import java.io.PrintStream;
+
+/**
+ * @class ConsolePlatformStrategy
+ *
+ * @brief Provides methods that define a platform-independent API for
+ *        output data to the console window and synchronizing on
+ *        thread completion in the ping/pong game.  It plays the role
+ *        of the "Concrete Strategy" in the Strategy pattern.
+ */
+public class ConsolePlatformStrategy extends PlatformStrategy
+{
+    /**
+     * Latch to decrement each time a thread exits to control when the
+     * play() method returns.
+     */
+    private static CountDownLatch mLatch = null;
+
+    /** Contains information for outputting to console window. */
+    PrintStream mOutput;
+
+    /** Ctor. */
+    ConsolePlatformStrategy(Object output) 
+    {
+        mOutput = (PrintStream) output;
+    }
+	
+    /** Do any initialization needed to start a new game. */
+    public void begin()
+    {
+        mLatch = new CountDownLatch(NUMBER_OF_THREADS);
+    }
+
+    /** Print the outputString to the display. */
+    public void print(String outputString)
+    {
+        /** Print to the console window. */
+        System.out.println(outputString);
+    }
+
+    /** Indicate that a game thread has finished running. */
+    public void done()
+    {
+        mLatch.countDown();
+    }
+    
+    /** Barrier that waits for all the game threads to finish. */
+    public void awaitDone()
+    {
+        try {
+            mLatch.await();
+        } catch(java.lang.InterruptedException e) {
+        }
+    }
+
+    /** Returns a string revealing the platform in use. */
+    public String platformName() 
+    {
+        return System.getProperty("java.specification.vendor");
+    }
+	
+    /**
+     * Error log formats the message and displays it for the debugging
+     * purposes.
+     */
+    public void errorLog(String javaFile, String errorMessage) 
+    {
+        System.out.println(javaFile + " " + errorMessage);
+    }
+}
