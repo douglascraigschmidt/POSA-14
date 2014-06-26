505408431f9a4f633e6b22cc42bbe0d6a70dcfd5
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-06-26 07:29:21 -0500

    fixed threadpooldownloadservice.

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices-Tests/src/edu/vuum/mocca/test/ThreadPoolDownloadServiceTests.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices-Tests/src/edu/vuum/mocca/test/ThreadPoolDownloadServiceTests.java
@@ -10,7 +10,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.test.ServiceTestCase;
-import edu.vuum.mocca.DownloadIntentService;
 import edu.vuum.mocca.DownloadUtils;
 import edu.vuum.mocca.ThreadPoolDownloadService;
 
@@ -141,9 +140,9 @@ public class ThreadPoolDownloadServiceTests
         Thread.sleep(Options.SHORT_WAIT_TIME);	
 
         //Create an Intent to start the service
-        mIntent = DownloadIntentService.makeIntent(getContext(), 
-                mHandler, 
-                Options.TEST_URI);        
+        mIntent = ThreadPoolDownloadService.makeIntent(getContext(), 
+                                                       mHandler, 
+                                                       Options.TEST_URI);        
 
         // Start the service
         startService(mIntent);
