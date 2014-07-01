340d3fba87725e5d162dc3b75d51f5f5342d0116
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-07-01 12:11:12 -0500

    Added comments.

--- ex/AcronymApplication/src/edu/vuum/mocca/GenericServiceConnection.java
+++ ex/AcronymApplication/src/edu/vuum/mocca/GenericServiceConnection.java
@@ -6,41 +6,51 @@ import android.os.IBinder;
 import android.util.Log;
 
 /**
- * The main Activity that launches the screen users will see.
+ * @class GenericServiceConnection
+ *
+ * @brief This class provides a generic framework for defining a
+ *        ServiceConnection object to an AIDLInterface that resides in
+ *        a Bound Service.  It factors out common behavior that is
+ *        otherwise written in a tedious and error-prone way for each
+ *        ServiceConnection object.  It would be cleaner if Java
+ *        generics supported traits and signature-based typing the way
+ *        that C++ templates do..
  */
-
-class GenericServiceConnection<InterfaceType> implements ServiceConnection {
+class GenericServiceConnection<AIDLInterface> implements ServiceConnection {
     /**
      * InterfaceFactory defines a factory method that converts the
-     * IBinder service into the appropriate AIDL interface object.
+     * IBinder service into the appropriate AIDL interface object.  We
+     * need to use this factory since Java generics lack the traits
+     * mechanisms and signature-based typing that C++ templates
+     * provide.
      */
-    interface InterfaceFactory<InterfaceType> {
-        public InterfaceType asInterface(IBinder service);
+    interface InterfaceFactory<AIDLInterface> {
+        public AIDLInterface asInterface(IBinder service);
     }
 
     /**
-     * Reference to the object after the client has finished binding
-     * to the Bound Service.
+     * Reference to the AIDL interface object after the client has
+     * finished binding to the Bound Service.
      */
-    private InterfaceType mInterface;
+    private AIDLInterface mInterface;
 
     /**
      * Instance that implements the InterfaceFactory interface to
      * return the appropriate AIDL interface object.
      */
-    private InterfaceFactory<InterfaceType> mInterfaceFactory;
+    private InterfaceFactory<AIDLInterface> mInterfaceFactory;
 
     /**
-     * Accessor that returns the interface.
+     * Accessor that returns the AIDL interface object.
      */
-    public InterfaceType getInterface() {
+    public AIDLInterface getInterface() {
         return mInterface;
     }
 
     /**
      * Constructor stores the InterfaceFactory for later use.
      */
-    GenericServiceConnection(InterfaceFactory<InterfaceType> i) {
+    GenericServiceConnection(InterfaceFactory<AIDLInterface> i) {
     	super();
         mInterfaceFactory = i;
     }
@@ -53,7 +63,7 @@ class GenericServiceConnection<InterfaceType> implements ServiceConnection {
     public void onServiceConnected(ComponentName name,
                                    IBinder service) {
         Log.d("GenericServiceConnection", "Connected to ComponentName " + name);
-        // Cast the returned IBinder object to the InterfaceType and
+        // Cast the returned IBinder object to the AIDLInterface and
         // store it for later use in mInterface.
         mInterface = mInterfaceFactory.asInterface(service);
     }
