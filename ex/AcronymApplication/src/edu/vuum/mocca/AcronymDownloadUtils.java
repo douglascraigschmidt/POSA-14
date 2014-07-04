09fdeba116798ed4bc48130f9b63e338754f36bd
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-07-03 20:18:29 -0500

    Finished the JSON ResponseHandler integration.

--- ex/AcronymApplication/src/edu/vuum/mocca/AcronymDownloadUtils.java
+++ ex/AcronymApplication/src/edu/vuum/mocca/AcronymDownloadUtils.java
@@ -1,19 +1,20 @@
 package edu.vuum.mocca;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import android.net.http.AndroidHttpClient;
 import android.util.Log;
 
 /**
@@ -22,8 +23,14 @@ import android.util.Log;
  */
 public class AcronymDownloadUtils {
     // Logging Tag
-    private final static String LOG_TAG = AcronymDownloadUtils.class
+    private final static String TAG = AcronymDownloadUtils.class
         .getCanonicalName();
+
+    /**
+     * Object that can invoke HTTP GET requests on URLs.
+     */
+    private final static AndroidHttpClient mClient =
+        AndroidHttpClient.newInstance("");
 
     /** 
      * URL to the Acronym web service.
@@ -32,93 +39,79 @@ public class AcronymDownloadUtils {
         "http://www.nactem.ac.uk/software/acromine/dictionary.py?sf=";
 
     /**
+     * A Handler that encapsulates the process of generating a
+     * response object from a HttpResponse.
+     */
+    static class JSONResponseHandler implements ResponseHandler<List<AcronymData>> {
+        /**
+         * This factory method converts the HttpResponse received from
+         * the GeoNames Web service into a List of AcronymData.
+         */
+        @Override
+        public List<AcronymData> handleResponse(HttpResponse response)
+            throws ClientProtocolException, IOException {
+
+            // Stores the processed results we get back from the
+            // GeoNames Web service.
+            List<AcronymData> result = new ArrayList<AcronymData>();
+
+            // A ResponseHandler that returns the response body as a
+            // String for successful (2xx) responses.
+            String JSONResponse =
+                new BasicResponseHandler().handleResponse(response);
+
+            try {
+                // Takes a JSON source string and extracts characters
+                // and tokens from it.
+                JSONArray object =
+                    (JSONArray) new JSONTokener(JSONResponse).nextValue();
+
+                // Get the JSON array of acronym results, marked with
+                // the 'lfs' name.
+                final JSONArray options = object.getJSONObject(0).getJSONArray("lfs");
+
+                // For each option, create an AcronymData and add it
+                // to rValue.
+                for (int i = 0; i < options.length(); i++) {
+                    JSONObject jsonObject = options.getJSONObject(i);
+                    result.add(new AcronymData(jsonObject.getString("lf"),
+                                               jsonObject.getInt("freq"),
+                                               jsonObject.getInt("since")));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+    }
+
+    /**
      * Obtain the Acronym information.
      * 
      * @return The information that responds to your current acronym search.
      */
     public static List<AcronymData> getResults(final String acronym) {
-        // Create the return value, which is a list of AcronymData
-        // objects.
-        final List<AcronymData> rValue =
-            new ArrayList<AcronymData>();
+        // Object that encapsulates the HTTP GET request to the
+        // Acronym Web service.
+        HttpGet request = new HttpGet(ACRONYM_URL + acronym);
 
-        // Try to access the Internet and parse the JSON reply.
+        // Factory method that converts the JSON string returned from
+        // the Acronym Web service into a List of AcronymData.
+        JSONResponseHandler responseHandler = 
+            new JSONResponseHandler();
+
         try {
-            // Go the URL and read the JSON array from it, using the
-            // readJsonArrayFromUrl() helper method.
-            final JSONArray obj = readJsonArrayFromUrl(ACRONYM_URL + acronym);
-
-            // Get the JSON array of results, marked with the 'lfs'
-            // name.
-            final JSONArray options = obj.getJSONObject(0).getJSONArray("lfs");
-
-            // For each option, create an AcronymData and add it to
-            // rValue.
-            for (int i = 0; i < options.length(); i++) {
-                JSONObject jsonObject = options.getJSONObject(i);
-                rValue.add(new AcronymData(jsonObject.getString("lf"),
-                                           jsonObject.getInt("freq"),
-                                           jsonObject.getInt("since")));
-            }
-
-            // Log for debugging purposes.
-            Log.d(LOG_TAG, "" + obj.getJSONObject(0).getJSONArray("lfs").length());
-
-        } catch (JSONException e) {
-            Log.e(LOG_TAG, "JSONException: " + e.getMessage());
+            // Get expanded acronyms from the Web service in JSON
+            // format, parse data into a List of AcronymData, and
+            // return the results.
+            return mClient.execute(request,
+                                   responseHandler);
+        } catch (ClientProtocolException e) {
+            Log.i(TAG, "ClientProtocolException");
         } catch (IOException e) {
-            Log.e(LOG_TAG, "IOException: " + e.getMessage());
+            Log.i(TAG, "IOException");
         }
-
-        return rValue;
+        return null;
+        
     }
-
-    /**
-     * Helper method that reads a URL and returns a JSONArray of the
-     * data, which only works on a URL to a Web service that returns
-     * JSON in an array format.
-     * 
-     * No defensive programming in this exmaple, for brevity, in
-     * production code you would do a lot more checks, etc.
-     */
-    public static JSONArray readJsonArrayFromUrl(String url)
-        throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
-            // Create a BufferedReader containing the contents
-            // downloaded from the Acronym Web service.
-            BufferedReader rd =
-                new BufferedReader(new InputStreamReader
-                                   (is,
-                                    Charset.forName("UTF-8")));
-
-            // Convert the java.io.Reader parameter to a String.
-            String jsonText = readAll(rd);
-
-            // Create a JSONarray from the String.
-            return new JSONArray(jsonText);
-        } finally {
-            is.close();
-        }
-    }
-
-    /**
-     * Convert the java.io.Reader parameter to a String.
-     * 
-     * Helper Method for readJsonArrayFromUrl.
-     * 
-     * @param rd
-     *            java.io.Reader to read from
-     * 
-     * @return String composed of the characters from the Reader.
-     */
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
-
 }
